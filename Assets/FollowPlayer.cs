669968d19f73c982af261ce2db2504fe4ece773d
monikanayak <67167270+monikanayak@users.noreply.github.com>
2021-11-19 23:40:08 +0530

    Initial commit

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform player;
+
+    void Update ()
+    {
+        if (player.position.y > transform.position.y)
+        {
+            transform.position = new Vector3(player.position.x, player.position.y, player.position.z);
+        }
+    }
+
+
+
+}
